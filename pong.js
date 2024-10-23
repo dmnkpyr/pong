3aac1ec98e96aaedf28305cc2d51e469cefe52e7
dmnkpyr <dompey@outlook.com>
2024-10-23 22:59:29 +0200

    pong basis setup

--- pong.js
+++ pong.js
@@ -1,5 +1,5 @@
 var xBall = Math.floor(Math.random() * 300) + 50;
-var yBall = 50;
+var yBall = Math.floor(Math.random() * 300) + 50;
 var xSpeed = (2, 7);
 var ySpeed = (-7, -2);
 var score = 0
@@ -18,18 +18,28 @@ function draw() {
   text("Score: " + score, 10, 25);
 
 
-  rect(0,windowHeight-15, 90, 15);
+  rect(mouseX-45,windowHeight-15, 90, 15);
   // TO DO 1: Bringe den Balken dazu der Maus auf der x-Achse zu folgen. 
 
   xBall += xSpeed;
-  ellipse(xBall, windowHeight/2, 20, 20);
+  yBall += ySpeed;
+  ellipse(xBall, yBall, 20, 20);
   // TO DO 2: Schaffst du es, dass sich der Ball frei bewegt?
 
-  if(xBall>windowWidth-10){
+  if(xBall>windowWidth-10 || xBall<10){
     xSpeed *= -1;
   } 
+
+  if(yBall<10){
+    ySpeed *= -1
+  }
   // TO DO 3: Lass den Ball von den Seitenrändern abprallen (windowWidth/windowHeight)
 
+  if(yBall>windowHeight-25){
+    if(xBall > mouseX-45 && xBall < mouseX+45){
+      ySpeed *= -1
+    }
+  }
   // TO DO 4: Lass den Ball vom Balken aprallen, falls sie sich berühren
 
 }
