1c7810894a2f856b6c242bf1e3a4f4bf34fd5af9
dmnkpyr <dompey@outlook.com>
2024-10-27 18:25:12 +0100

    pong 3 change colors

--- pong3.js
+++ pong3.js
@@ -2,40 +2,39 @@
 var dBall = 100
 var xBall = Math.floor(Math.random() * 300) + 50;
 var yBall = -dBall
-var minSpeed = 4
+var minSpeed = 8
 var maxSpeed = 16
 var xSpeed = Math.floor(Math.random() * (maxSpeed-minSpeed)) + minSpeed
 var ySpeed = Math.floor(Math.random() * (maxSpeed-minSpeed)) + minSpeed
 var startCounter = 0
-var initialHue = Math.random(1000)
+var initialHue = Math.random()
 var color1 = {
   h: initialHue,
-  s: 500,
-  b: 1000
+  s: 0.8,
+  b: 0.1
 }
 var bgOpacity = 1000
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 1000)
+  colorMode(HSB, 1)
 }
 
 function draw() {
-  background((color1.h)%1000,500, 1000, bgOpacity);
+  background(color1.h, 0.4, 0.2, bgOpacity);
   noStroke()
 
   fill(color1.h, color1.s, color1.b);
-  xBall += xSpeed;
+  xBall += xSpeed* noise(0.01 * frameCount);
   yBall += ySpeed;
-  color1.h = (color1.h+1)%1000
-  color1.s += 1
+  color1.b = map(sin(frameCount/100),-1,1,0.2,1)
   startCounter += 1
   ellipse(xBall, yBall, dBall, dBall);
 
-  fill(0,0,600)
+  fill(color1.h, 0.2, 0.8)
   rect(0,height-15,width,15)
-  fill(0,0,100)
+  fill(color1.h, 0.4, 0.2)
   rect(mouseX-45,windowHeight-15, 90, 15);
 
   bgOpacity = 0
@@ -47,19 +46,19 @@ function draw() {
   if(yBall<dBall/2 && startCounter>40){
     ySpeed *= -1
   }
-
   if(yBall>windowHeight-dBall/2){
     if(xBall > mouseX-45 && xBall < mouseX+45){
       ySpeed *= -1
-      color1.h = random((initialHue-100)%1000, (initialHue+100)%1000)
-      color1.s = 500
     }else if(yBall>windowHeight+dBall/2) {
       bgOpacity = 1000
       yBall = -dBall
       startCounter = 0
       xSpeed = Math.floor(Math.random() * (maxSpeed-minSpeed)) + minSpeed
       ySpeed = Math.floor(Math.random() * (maxSpeed-minSpeed)) + minSpeed
-      initialHue = Math.random(1000)
+      initialHue = Math.random()
+      console.log(initialHue)
+      color1.h = initialHue
+      color1.s = 0.8
     }
   }
   
